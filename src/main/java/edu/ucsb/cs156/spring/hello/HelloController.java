05dc9070e324ff9fea7022fb2cb71d4fb38db9e6
Tim Qin <yqin@ucsb.edu>
2023-04-20 12:42:17 -0700

    changed greetings

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from yqin01!";
+        return "Greetings from yqin!";
     }
     
 }
